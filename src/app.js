d9b6afb8e77f5abd0d0ae4da4eb2e8e9937b714a
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-11-05 02:31:54 -0600

    src/app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
+app.use((req, res, next) => {
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;
