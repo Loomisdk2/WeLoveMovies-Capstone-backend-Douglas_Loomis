5b034af9b521adfd9498711799034cee6ac54827
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-11-05 02:08:51 -0600

    src/movies/movies.controller

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -9,3 +9,71 @@ async function movieExists(req, res, next) {
   }
   next({ status: 404, message: "Movie cannot be found." });
 }
+
+async function list(req, res) {
+  const { is_showing } = req.query;
+
+  if (is_showing === "true") {
+    const movies = await moviesService.listShowingMovies();
+    res.json({ data: movies });
+  } else {
+    const movies = await moviesService.list();
+    res.json({ data: movies });
+  }
+}
+
+function read(req, res, next) {
+  const { movie: data } = res.locals;
+  res.json({ data });
+}
+
+function listTheaters(req, res) {
+  const { movieId } = req.params;
+
+  knex("theaters as t")
+    .select("t.*")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .where({ "mt.movie_id": movieId, "mt.is_showing": true })
+    .then((theaters) => {
+      res.json({ data: theaters });
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching theaters." });
+    });
+}
+
+function listMovieReviews(req, res) {
+  const { movieId } = req.params;
+
+  knex("reviews as r")
+    .join("critics as c", "r.critic_id", "c.critic_id")
+    .select("r.*", "c.*")
+    .where({ "r.movie_id": movieId })
+    .then((reviews) => {
+      const reviewsWithCriticDetails = reviews.map((review) => {
+        const { critic_id, ...criticInfo } = review;
+        return {
+          ...review,
+          critic: criticInfo,
+        };
+      });
+
+      res.json({ data: reviewsWithCriticDetails });
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching movie reviews." });
+    });
+}
+
+module.exports = {
+  list,
+  read: [movieExists, read],
+  listTheaters,
+  listMovieReviews,
+};
